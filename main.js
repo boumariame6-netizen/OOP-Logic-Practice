509deac9f0f730409b51229a00b791f6fd8a7584
Mariame Boughamir <boumariame6@gmail.com>
2026-03-02 01:17:06 +0000

    all done

--- main.js
+++ main.js
@@ -194,3 +194,35 @@ String.prototype.addLove = function(){
 // Do Not Edit Below
 let myStr = "Elzero";
 console.log(myStr.addLove()); // I Love Elzero Web School
+
+
+
+
+
+const myObj = {
+  username: "Elzero",
+  id: 100,
+  score: 1000,
+  country: "Egypt",
+};
+
+// Write Your Code Here
+Object.defineProperty(myObj ,user,{
+  writable:false,
+  enumerable:false,
+
+})
+
+myObj.score = 500;
+
+for (let prop in myObj) {
+  console.log(`${prop} => ${myObj[prop]}`);
+}
+
+console.log(myObj);
+
+// Needed Output
+
+"username => Elzero"
+"score => 1000"
+// {username: 'Elzero', score: 1000, id: 100}
